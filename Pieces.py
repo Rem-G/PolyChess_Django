5fba9ee62b4ba253fec1fba8f24dd4c7604e56a9
Thomas-CENCI <thcenci@gmail.com>
2019-11-28 19:50:44 +0100

    Ajout des fonctions présentes dans le fichier Pion.py

--- Pieces.py
+++ Pieces.py
@@ -3,15 +3,51 @@ class Pion():
 		self.nom = nom
 		self.position = pos_initiale
 		self.id = 0
+		self.firstMove = True
 
-	def set_piece_id(self, pieceId):
-		self.id = pieceId
+	def set_piece_id(self, identifier):
+		"""
+		:param pieceId type int: Identifiant du pion
+		Set l'identifiant du pion
+		"""
+		self.id = identifier
 
 	def get_piece_position(self):
 		return self.position
 
 	def set_piece_position(self, position):
+		"""
+		:param position type (str, int): Coordonnees de la destination
+		Met à jour la position du pion
+		"""
 		self.position = position
+
+	def firstMoveOver(self):
+		"""
+		Permet de mettre à jour l'etat du 1er tour du pion
+		"""
+		self.firstMove = False
+
+	def pawnPossibleMoves(self):
+		"""
+		:return type list: Liste des moves possibles pour le pion
+		"""
+		Destination = []
+
+		if self.nom == 'p':
+			Destination.append((self.position[0], self.position[1] + 1))
+			if self.firstMove:
+				Destination.append((self.position[0], self.position[1] + 2))
+				self.firstMoveOver()
+
+		if self.nom == 'P':
+			Destination.append((self.position[0], self.position[1] - 1))
+			if self.firstMove:
+				Destination.append((self.position[0], self.position[1] - 2))
+				self.firstMoveOver()
+
+		return Destination
+
 
 class Tour():
 	def __init__(self, nom, pos_initiale):
