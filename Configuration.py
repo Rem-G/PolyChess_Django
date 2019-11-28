3ba41d3210510f181e27e13b5497b49f530b1e5d
Remi <remigosselin99@yahoo.fr>
2019-11-28 19:47:23 +0100

    Correctifs mineurs + documentation des fonctions

--- Configuration.py
+++ Configuration.py
@@ -6,28 +6,35 @@ class GeneralConf():
 	def __init__(self):
 		self.pieces = list()
 		self.msg_error = list()
-
+		self.board = Mat64()
 
 	def add_piece(self, piece):
+		"""
+		Ajoute une nouvelle pièce à la liste de pièces existantes
+		"""
 		piece.set_piece_id(len(self.pieces))
 		self.pieces.append(piece)
 
 
 	def matrice_affichage(self):
 		"""
+		Génère la matrice d'interface utilisateur en supprimant les lignes de -1 et affichant le nom de chaque pièce en fonction de leur emplacement
+		sur l'échiquier 
+		:return matrice_screen: Matrice 8*8 avec le nom des pièces affiché sur leur position, les -1 de la matrice initiale sont convertis en '.'
 		"""
 		pieces = self.pieces
 
-		matrice = Mat64().matrice_init()
+		matrice = self.board.matrice_init()
 
 		for piece in pieces:
-			pos = Mat64().position_piece([piece.get_piece_position()[0], piece.get_piece_position()[1]])
+			pos = self.board.position_piece_mat([piece.get_piece_position()[0], piece.get_piece_position()[1]])
 			matrice[pos[0]][pos[1]] = piece.nom + ' '
 
 		for index_i, i in enumerate(matrice):
-			matrice[index_i] = i[1:len(i)-1]
+			matrice[index_i] = i[1:len(i)-1] #Suppression du premier et dernier élément de la ligne -> suppression des -1
 			for index_j, j in enumerate(i):
-				try:
+				try: #Chaque ligne de la matrice étant redéfinie au dessus en supprimant la première et la dernière valeur
+					# le maximum d'index_i est plus élevé que la taille de la nouvelle ligne
 					if matrice[index_i][index_j] == -1:
 						matrice[index_i][index_j] = '. '
 				except:
@@ -40,22 +47,26 @@ class GeneralConf():
 
 	def deplacement_piece(self, pos_depart, pos_arrivee, upper):
 		"""
-		Ajouter verification par rapport à matrice jeu
-		SI position voulue différente de -1
+		Change de position une pièce selon la décision du joueur
+
+		:param pos_depart: Position initiale de la pièce à bouger
+		:param pos_depart: Position de destination de la pièce à bouger
+		:param upper: Vérification du joueur faisant la requête : upper == True -> joueur blanc
 		"""		
 		pos_depart[1] = str(pos_depart[1]) #Conversion coordonée int en str
 		pos_arrivee[1] = str(pos_arrivee[1])
 
-		mat_jeu = Mat64().matrice_jeu()
+		pos_depart_convert = self.board.position_piece_mat(pos_depart) #Conversion des coordonnées utilisateur de la pièce en coordonnées de la matrice de jeu
+		pos_arrivee_convert = self.board.position_piece_mat(pos_arrivee)
 
-		pos_depart = Mat64().position_piece(pos_depart)
-		pos_arrivee_convert = Mat64().position_piece(pos_arrivee)
-
-		if mat_jeu[pos_depart[0]][pos_depart[1]] != -1 and mat_jeu[pos_arrivee_convert[0]][pos_arrivee_convert[1]] != -1:
+		#Vérification que la valeur de la position de départ dans la matrice de jeu est différente de 1
+		if self.board.valeur_position_piece_mat(pos_depart_convert) != -1 and self.board.valeur_position_piece_mat(pos_arrivee_convert) != -1:
 			for piece in self.pieces:
-				piece_x, piece_y = Mat64().position_piece(piece.get_piece_position())[0], Mat64().position_piece(piece.get_piece_position())[1]
+				#Récupération et conversion des coordonnées utilisateur de la pièce en coordonnées de la matrice de jeu
+				piece_x, piece_y = self.board.position_piece_mat(piece.get_piece_position())[0], self.board.position_piece_mat(piece.get_piece_position())[1]
 
-				if piece_x == pos_depart[0] and piece_y == pos_depart[1]:
+				if piece_x == pos_depart_convert[0] and piece_y == pos_depart_convert[1]:
+					#Vérification de la position actuelle de la pièce et de la position de départ demandée par l'utilisateur
 					if upper is True:
 						#Tour du joueur blanc
 						if piece.nom.isupper():
