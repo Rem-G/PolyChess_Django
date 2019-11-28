be14c1f1857b1f5649a79dd303975b96e9dd1049
Remi <remigosselin99@yahoo.fr>
2019-11-28 14:32:03 +0100

    Ajout déplacement pièces, mise en place d'une partie à 2 joueurs

---
+++ Pieces.py
@@ -0,0 +1,92 @@
+class Pion():
+	def __init__(self, nom, pos_initiale):
+		self.nom = nom
+		self.position = pos_initiale
+		self.id = 0
+
+	def set_piece_id(self, pieceId):
+		self.id = pieceId
+
+	def get_piece_position(self):
+		return self.position
+
+	def set_piece_position(self, position):
+		self.position = position
+
+class Tour():
+	def __init__(self, nom, pos_initiale):
+		self.nom = nom
+		self.position = pos_initiale
+		self.id = 0
+
+	def set_piece_id(self, pieceId):
+		self.id = pieceId
+
+	def get_piece_position(self):
+		return self.position
+
+	def set_piece_position(self, position):
+		self.position = position
+
+
+class Cavalier():
+	def __init__(self, nom, pos_initiale):
+		self.nom = nom
+		self.position = pos_initiale
+		self.id = 0
+
+	def set_piece_id(self, pieceId):
+		self.id = pieceId
+
+	def get_piece_position(self):
+		return self.position
+
+	def set_piece_position(self, position):
+		self.position = position
+
+class Fou():
+	def __init__(self, nom, pos_initiale):
+		self.nom = nom
+		self.position = pos_initiale
+		self.id = 0
+
+	def set_piece_id(self, pieceId):
+		self.id = pieceId
+
+	def get_piece_position(self):
+		return self.position
+
+	def set_piece_position(self, position):
+		self.position = position
+
+class Dame():
+	def __init__(self, nom, pos_initiale):
+		self.nom = nom
+		self.position = pos_initiale
+		self.id = 0
+
+	def set_piece_id(self, pieceId):
+		self.id = pieceId
+
+	def get_piece_position(self):
+		return self.position
+
+	def set_piece_position(self, position):
+		self.position = position
+
+
+class Roi():
+	def __init__(self, nom, pos_initiale):
+		self.nom = nom
+		self.position = pos_initiale
+		self.id = 0
+
+	def set_piece_id(self, pieceId):
+		self.id = pieceId
+
+	def get_piece_position(self):
+		return self.position
+
+	def set_piece_position(self, position):
+		self.position = position
+
--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+from Pieces import Pion
+
 class Mat64():
 	def __init__(self):
 		pass
@@ -9,7 +11,7 @@ class Mat64():
 		"""
 		return [[-1]*10 for i in range(12)]
 
-	def game_aera(self):
+	def matrice_jeu(self):
 		"""
 		-1 représente une zone non accessible par une pièce de jeu
 		:return new_board: Echequier de taille 12*10 pour une surface de jeu de 8*8 permettant de gêrer les débordements
@@ -30,25 +32,22 @@ class Mat64():
 		return new_board
 
 
-	def position_init_pieces(self, piece):
-		"""
-		:param piece: position initiale de la piece
-		:return nom_piece: Renvoie le nom de la pièce en fonction de sa position initiale passée en paramètre
-		"""
-		pieces = {'P': [48,49,50,51,52,53,54,55], 'p': [8, 9, 10, 11, 12, 13, 14, 15], 'T' : [56, 63], 't' : [0, 7], 'C' : [57, 62], 'c' : [1, 6], 'F' : [58, 61], 'f' :[2, 5], 'D' : [59], 'd' : [3], 'R' : [60], 'r' : [4] }
-		
-		for nom_piece, pos_piece in pieces.items():
-			if piece in pos_piece:
-				return nom_piece
-
-	def position_piece(self, piece):
+	def position_piece(self, piece_pos):
 		"""
 		:return list: Renvoie la position de la pièce dans le référentiel de la matrice de l'échéquier
 		"""
-		lignes_matrice = {'h' : 2, 'g' : 3, 'f' : 4, 'e' : 5, 'd': 6, 'c' : 7, 'b' : 8, 'a' : 9}
-		colonnes_matrice = {'8' : 1, '7' : 2, '6' : 3, '5' : 4, '4' : 5, '3' : 6, '2' : 7, '1' : 8 }
+		lignes_matrice = {'8' : 2, '7' : 3, '6' : 4, '5' : 5, '4': 6, '3' : 7, '2' : 8, '1' : 9}
+		colonnes_matrice = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7, 'h' : 8 }
 
-		board = self.game_aera()
+		board = self.matrice_jeu()
 
-		return [colonnes_matrice[piece[1]], lignes_matrice[piece[0]]]
+		if piece_pos[0] in colonnes_matrice.keys() and piece_pos[1] in lignes_matrice.keys():
+			return [lignes_matrice[piece_pos[1]], colonnes_matrice[piece_pos[0]]]
+
+
+	def position_piece_mat(self, piece_pos):
+		"""
+		"""
+		matrice = self.matrice_jeu()
+		return matrice[piece_pos[0]][piece_pos[1]]
 
